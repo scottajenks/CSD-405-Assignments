53e8868cd19750575a0c0541260633a05bd5ed02
Scott Jenks <scottajenks@gmail.com>
2023-01-24 18:46:15 -0500

    Upload files

--- Jenks-Module5-Assignment/src/Program1.java
+++ Jenks-Module5-Assignment/src/Program1.java
@@ -1,5 +1,53 @@
+/*Scott Jenks
+ * 1/24/2023
+ * Module 5 - ArrayList Assignment with Exceptions
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Program1 {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        // Create list to store coding terms
+        ArrayList<String> codingTerms = new ArrayList<>();
+
+        // Add coding terms to list
+        codingTerms.add("Computer");
+        codingTerms.add("Java");
+        codingTerms.add("Python");
+        codingTerms.add("Method");
+        codingTerms.add("Variable");
+        codingTerms.add("String");
+        codingTerms.add("Boolean");
+        codingTerms.add("Mouse");
+        codingTerms.add("Coffee");
+        codingTerms.add("Git Hub");
+
+        // Print out list
+        for (String i : codingTerms) {
+            System.out.println(i);
+        }
+
+        // Ask user which word they would like to see again
+        Scanner input = new Scanner(System.in);
+        System.out.println("\nWhich coding term would you like to see again?");
+        String word = input.nextLine();
+
+        // Look through list to find users word
+        try {
+            // if codingTerms arrayList contins users word
+            codingTerms.contains(word);
+            // find index location of word
+            Integer wordLocation = codingTerms.indexOf(word);
+            // print the word in the index location
+            System.out.println("\n" + codingTerms.get(wordLocation) + " is in the list!");
+        } catch (Exception e) {
+            System.out.println("\nOut of Bounds");
+        }
+
+        // Close Scanner
+        input.close();
     }
 }
---
+++ Jenks-Module5-Assignment/src/Program2.java
@@ -0,0 +1,79 @@
+/*Scott Jenks
+ * 1/24/2023
+ * Module 5 - Create, write and read file Assignment
+ */
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program2 {
+
+    public static void main(String[] args) {
+
+        // Create file data.file if it does not exist
+        try {
+            File newFile = new File("data.file");
+            if (newFile.createNewFile()) {
+                System.out.println("File " + newFile.getName() + " has been created.");
+
+                // Write 10 randomly generated numbers to file
+                try {
+                    FileWriter writeInNewFile = new FileWriter("data.file");
+
+                    for (int i = 0; i < 10; i++) {
+                        int randomNumber = (int) (Math.random() * 100);
+                        writeInNewFile.write(randomNumber + " ");
+                    }
+                    System.out.println("10 numbers were selected at random and added to " + newFile.getName());
+
+                    // Close file
+                    writeInNewFile.close();
+                } catch (IOException e) {
+                    System.out.println("An error occurred.");
+                    e.printStackTrace();
+                }
+
+            } else {
+                System.out.println("The file " + newFile.getName() + " already exists.");
+
+                // Append 10 randomly generated numbers to file
+                try {
+                    FileWriter writeInNewFile = new FileWriter("data.file", true);
+
+                    for (int i = 0; i < 10; i++) {
+                        int randomNumber = (int) (Math.random() * 100) + 1;
+                        writeInNewFile.write(randomNumber + " ");
+                    }
+                    System.out.println("10 MORE numbers were selected at random and added to " + newFile.getName());
+
+                    // Close file
+                    writeInNewFile.close();
+                } catch (IOException e) {
+                    System.out.println("An error occurred.");
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        // Reopen, read and display contents of file
+        try {
+            File openFile = new File("data.file");
+            Scanner readFile = new Scanner(openFile);
+            System.out.println("\nThe contents of the file " + openFile.getName() + " are:");
+            while (readFile.hasNext()) {
+                String data = readFile.next();
+                System.out.printf("%-4s", data);
+            }
+            // Close file
+            readFile.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
